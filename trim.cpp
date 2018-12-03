8dad1f36b0d033c21cd313b63b97d7db80738cd2
Kanuric Ekrem <if160114@WS8A09.local>
2018-12-03 13:31:01 +0100

    FINISH

--- trim.cpp
+++ trim.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Klasse: <your class>
+ *				HTBLA-Leonding / Klasse: 2AHIF
  * ---------------------------------------------------------
  * Exercise Number: 0
  * Title:			trim.cpp
@@ -14,5 +14,25 @@
 
 void trim	(const char* source,char* trimmed_string)
 {
+  //declaration of variables
+ int start = 0;
+ int end = strlen(source)-1;
+
+ //checks if source is empty
+ if (strlen(source) == 0) {
   strcpy(trimmed_string, source);
+  return;
+ }
+
+//get start of string
+ for (start; source[start] == ' '; start++){}
+
+//get end of string
+ for (end;source[end] == ' '; end--){}
+
+//copy string into trimmed_string
+ for (int i = 0; start <= end; i++) {
+  trimmed_string[i] = source[start];
+  start++;
+  }
 }
